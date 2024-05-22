9aae7b23c49ae115ce67dfe98df7ee14e3563477
NimraFatima <farimanim16@gmail.com>
2024-05-21 21:21:35 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let htmlQuiz =[
+    {
+       que: "1. Who is making web Standards?",
+       opt1: "Google",
+       opt2: "Microsoft",
+       opt3: "World Wide Web",
+       opt4: "Mozrella",
+       ans: "Worl Wide Web"
+    },
+
+    {
+        que: "2. What is the abbrevation of HTML?",
+        opt1: "Hyper Text Markup Language",
+        opt2: "Hyper Info Markup Language",
+        opt3: "Hyper test Markup Language",
+        opt4: "Hyper Tend Markup Language",
+        ans: "Hyper Text Markup Language"
+    },
+
+    {
+        que: "3.How many heading tags in HTML?",
+        opt1: "1",
+        opt2: "2",
+        opt3: "4",
+        opt4: "6",
+        ans: "6"
+    }
+]
+
+
+let questionCount = 0
+
+let score = 0
